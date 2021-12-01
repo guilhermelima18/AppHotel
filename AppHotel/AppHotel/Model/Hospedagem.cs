b1c2941f062bf28a23190382d92541b047a7aca4
guilhermelima18 <guilhermelima18@hotmail.com>
2021-12-01 15:48:03 -0300

    Adicionar arquivos de projeto.

---
+++ AppHotel/AppHotel/Model/Hospedagem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppHotel.Model
+{
+    public class Hospedagem
+    {
+        public Suite QuartoEscolhido { get; set; }
+        public int QtdeAdultos { get; set; }
+        public int QtdeCriancas { get; set; }
+        public DateTime DataCheckIn
+        {
+            get; set;
+        }
+        public DateTime DataCheckOut
+        {
+            get; set;
+        }
+        public int Estadia
+        {
+            get => DataCheckOut.Subtract(DataCheckIn).Days;
+        }
+        public double ValorTotal
+        {
+            get => ((QtdeAdultos * QuartoEscolhido.DiariaAdulto) + (QtdeCriancas * QuartoEscolhido.DiariaCrianca)) * Estadia;
+        }
+    }
+}
---
+++ AppHotel/AppHotel/View/ContratacaoHospedagem.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppHotel.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ContratacaoHospedagem : ContentPage
+    {
+        public ContratacaoHospedagem()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+    }
+}
